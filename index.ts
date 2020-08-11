69a79e7b6143637cd0934d98e9826573b3862eb4
Zach Ayers <ID+zjayers@users.noreply.github.com>
2020-08-11 12:14:12 -0500

    init

---
+++ index.ts
@@ -0,0 +1,45 @@
+/**
+ * How to measure Big O of an algorithm?
+ * ASYMPTOTIC ANALYSIS
+ * - 1. For a given algorithm - define the mathematical equation for its time complexity
+ * - - - > Count the number of expression executions (code-line executions)
+ * - - - > Determine pattern inside of function for various inputs
+ * - - - > How many times does each code-line run per input?
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - -
+ * - - - > EXAMPLE:                             Number of executions per line for n     - - -
+ * - - - >  function sumUp(n) {                 | n = 1 | n = 3 | n = 10 | n = n |      - - -
+ * - - - >     let result = 0;                      1       1       1       1           - - -
+ * - - - >                                          -       -       -       -           - - -
+ * - - - >     for (let i = 1; i <= n; i++) {       1       1       1       1           - - -
+ * - - - >         result += i;                     1       3       10      n           - - -
+ * - - - >     }                                    -       -       -       -           - - -
+ * - - - >                                          -       -       -       -           - - -
+ * - - - >     return result;                       1       1       1       1           - - -
+ * - - - >  }                                                                           - - -
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - -
+ * - - - >  Time Complexity = 1 + 1 + n + 1
+ * - - - >                    3 + (1 * n) - Simplify the formula
+ * - - - >                    a * n + b) - Replace constants with coefficients
+ *
+ * - 2. Find the FASTEST growing term
+ * - - - > [a * n]
+ *
+ * - 3. Drop the coefficients
+ * - - - > Time Complexity = n
+ * - - - > Final Notation: O(n)
+ *
+ * - > ------ Big O Types ------
+ * - > O (1).........Constant Time
+ * - > O (n).........Linear Time
+ * - > O (log n).....Logarithmic Time
+ * - > O (n^2).......Quadratic Time
+ * - > O (2^n).......Exponential time
+ */
+
+
+// performance Modules
+export {Monitor} from './performance/monitor'
+
+// Algorithm Modules
+export {SumUp} from './algorithms/sumUp';
+
---
+++ performance/monitor.ts
@@ -0,0 +1,38 @@
+import {performance} from 'perf_hooks';
+
+/**
+ * Class Representing a performance Monitor
+ * Functionality:
+ * - Start the time monitor to take a snapshot of the current DateTime [or an input DateTime].
+ * - Stop the time monitor to report the time elapsed since the monitor was started (in ms).
+ */
+class Monitor {
+    private start: number;
+    private end: number;
+    private reading: number;
+
+    /**
+     * Start The performance Monitor
+     * @param {number} [startDate=Date.now()] - The date to start measurement
+     * @returns {void}
+     */
+    public startMonitor(startDate?: number): void {
+        this.start = startDate || performance.now();
+    }
+
+
+    /***
+     * Stop the performance monitor and return the output time between starting and stopping the monitor
+     * @returns {number} - The amount of milliseconds the monitor was stopped minus the DateTime the monitor was started.
+     */
+    public stopMonitor(): number {
+        this.end = performance.now();
+        this.reading = this.end - this.start;
+        this.end = null;
+        this.start = null;
+        console.log(this.reading);
+        return this.reading;
+    }
+}
+
+export {Monitor};
